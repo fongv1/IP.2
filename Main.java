7bdcfc82187c8d2b9ad5f55587e2f9521975ff88
Vin <fongv1@hotmail.com>
2018-09-29 20:36:11 +0200

    Main edited

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
+        TaskOrganiser sda = new TaskOrganiser();
 
+        Task t1 = new Task("Boil water", 2018, 12, 1);
+        //t1.setDueDate(2018, 12, 23);
+        //System.out.println(t1.getDate().getDueDate());
+        //t1.getDate().printCreationDate();
+        //t1.getDate().setCreationDate();
+        Task t2 = new Task("make pasta", 2018, 11, 2);
+        Task t3 = new Task("make sauce", 2018, 11, 01);
+        Task t4 = new Task("make meatballs", 2019, 12, 2);
+        Task t5 = new Task("serve", 2010, 2,20) ;
+        sda.addTask(t1);
+        sda.addTask(t2);
+        sda.addTask(t3);
+        sda.addTask(t4);
+        sda.addTask(t5);
+
+        //sda.printAllTasks();
+        sda.orderByDate();
+        //t1.getDate().compareTo(t1.getDate().getDueDate());
     }
 }
