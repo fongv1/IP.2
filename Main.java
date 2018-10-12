af04a3994b205dc45ac7e26fe487f1ecb5db31a1
Vin <fongv1@hotmail.com>
2018-10-12 21:54:38 +0200

    Issue #79 closed. dates now handle leap years and 30 or 31 day months

--- Main.java
+++ Main.java
@@ -1,14 +1,10 @@
 import java.io.IOException;
-import java.util.Calendar;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-
-
-        Calendar test = Calendar.getInstance();
-        test.set(Calendar.MONTH, 4);
-        System.out.println(test.get(Calendar.MONTH));
+    Interface sda = new Interface();
+    sda.start();
 
     }
 }
--- TaskDate.java
+++ TaskDate.java
@@ -1,7 +1,7 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
+import java.util.Locale;
 
 /**
  * Task Organiser is a simple application which allows users to manage
@@ -18,12 +18,9 @@ import java.util.HashMap;
 
 public class TaskDate implements Serializable {
 
-    private static ArrayList<Integer> monthsWithThirtyDays = {3, 5, 8, 10};
-
+    private static ArrayList<Integer> monthsWithThirtyDays;
     private Calendar dueDate = Calendar.getInstance();
-    private HashMap<Integer, Integer> daysInMonth;
     private ArrayList<Integer> leapYears;
-
 
 
     /**
@@ -35,7 +32,7 @@ public class TaskDate implements Serializable {
     public TaskDate() {
         dueDate.clear();
         populateLeapYears();
-        populateDaysInMonth();
+        populateThirtyDayMonths();
     }
 
 
@@ -52,7 +49,9 @@ public class TaskDate implements Serializable {
      * Set the due date of a task relative to the parameters,
      * provided the parameters are valid. Month value has one
      * taken away from the users' input to fall in line with a
-     * 0 start to months (i.e. 0 is January).
+     * 0 start to months (i.e. 0 is January). It will check
+     * whether the parameters are valid through testFullDate
+     * method before setting the values.
      *
      * @param year  The year which the task is due
      * @param month The month which the  task is due
@@ -60,7 +59,7 @@ public class TaskDate implements Serializable {
      */
 
     public void setDueDate(int year, int month, int date) {
-        if (testDateParameters(year, month, date)) {
+        if (testFullDate(year, month - 1, date)) {
             dueDate.set(Calendar.YEAR, year);
             dueDate.set(Calendar.MONTH, month - 1);
             dueDate.set(Calendar.DATE, date);
@@ -69,94 +68,6 @@ public class TaskDate implements Serializable {
         } else {
             System.out.println(">> Error, incorrect due date parameters.");
         }
-    }
-
-
-    /**
-     * This tests if the user's calendar input is valid for the
-     * task's due date. Users will only be able to set tasks due
-     * from 2000 to 2100 (an arbitrary figure), because the date
-     * of a default/cleared Calendar object has a year of 1970.
-     *
-     * @param year  Test the year due date of the task
-     * @param month Test the month due date of the task
-     * @param date  Test the date of the due date of the task
-     * @return Will return true, if parameters are valid
-     */
-
-    private boolean testDateParameters(int year, int month, int date) {
-        boolean correct;
-
-        if (year < 2000 || year > 2100) {
-            correct = false;
-        }
-
-        else if (month < 1 || month > 12) {
-            correct = false;
-        }
-
-        else if (date < 1 || date > 31) {
-            correct = false;
-        }
-
-        else {
-            correct = true;
-        }
-
-        return correct;
-
-    }
-
-    /**
-     * A method to print out information on the dueDate field
-     * related to a task. It works in tandem with the checkIfDefault
-     * method to confirm if a dueDate is at its default state, or if
-     * it has been set by the user. It will shift the month by 1 when
-     * printed to the user, as the Calendar class has a field for
-     * month which starts at 0. It will also add a 0 to the date/month
-     * , if the date is less than 10, such that the String fits a
-     * YYYY-MM-DD format.
-     *
-     * @return A String based on the dueDate of a task
-     */
-
-    public String printDateString() {
-
-        String dateString;
-        String monthString;
-        String result;
-
-        if (checkIfDefaultDate()) {
-
-            result = "Not set";
-
-
-        } else {
-
-            int date = dueDate.get(Calendar.DATE);
-
-            if (date < 10) {
-                dateString = "0" + date;
-            } else {
-                dateString = date + "";
-            }
-
-            int month = dueDate.get(Calendar.MONTH) + 1;
-
-            if (month < 10){
-                monthString = "0" + month ;
-            }
-
-            else {
-                monthString = month + "";
-            }
-
-            result = dueDate.get(Calendar.YEAR) + "-" +
-                            monthString + "-" + dateString;
-
-        }
-
-        return result;
     }
 
     /**
@@ -184,68 +95,265 @@ public class TaskDate implements Serializable {
         return result;
     }
 
-    public boolean testDate(int year, int month, int date) {
+
+    /**
+     * This tests if the user's calendar input is valid for the
+     * task's due date. Users will only be able to set tasks due
+     * from 2000 to 2100 (an arbitrary figure). It only tests the
+     * boundaries of a user's due date values. It is used together
+     * with fullDateTest method.
+     *
+     * @param year  Test the year due date of the task
+     * @param month Test the month due date of the task
+     * @param date  Test the date of the due date of the task
+     * @return Will return true, if parameters are valid
+     */
+
+    private boolean testBorders (int year, int month, int date) {
+        boolean correct;
+
+        if (year < 2000 || year > 2100) {
+            correct = false;
+        } else if (month < 1 || month > 12) {
+            correct = false;
+        } else if (date < 1 || date > 31) {
+            correct = false;
+        } else {
+            correct = true;
+        }
+
+        return correct;
+
+    }
+
+    /**
+     * This method is the comprehensive test for a users's input for a
+     * due date. It will cover leap years/ordinary years, days of the
+     * month in February and the rest of months of a year. It works in
+     * conjunction with a number of other methods int his class.
+     *
+     * @param year Test the year due date of the task
+     * @param month Test the month due date of the task
+     * @param date Test the date of the due date of the task
+     * @return Will return true, if parameters are valid
+     */
+    public boolean testFullDate(int year, int month, int date) {
         boolean correct = false;
         int february = 1;
-        Calendar tempCal = Calendar.getInstance();
 
-        if (leapYears.contains(year))
+
+        if (testBorders(year, month, date)) {
+
+            if (leapYears.contains(year)) {
+
+                if (month == february) {
+                    correct = handlesLeapFebruary(year, date);
+                }
+
+                else {
+                    correct = handlesMonthsAndDays(month, date);
+                }
+            }
+
+            // Not a leap year
+            else {
+
+                if (month == february) {
+                    correct = handlesFebruary(year, date);
+                }
+
+                else {
+                    correct = handlesMonthsAndDays(month, date);
+                }
+            }
+        }
+
+        // Due dates are not within limits of a due date
+        else {
+            System.out.println(">> Years can only be between 2000 and 2100 (inclusive)");
+            System.out.println(">> Months can only be between 1 and 12 (inclusive)");
+            System.out.println(">> Dates can only be between 1 and 31 (inclusive)");
+        }
+
+        return correct;
+    }
+
+    /**
+     * This method handles if the date a user has given is correct
+     * to the amount of days in a normal 28 day February. It is
+     * used as a part of the testFullDate method.
+     *
+     * @param year Test the year due date of the task
+     * @param date Test the date of the due date of the task
+     * @return Will return true, if parameters are valid
+     */
+
+    public boolean handlesFebruary (int year, int date) {
+        boolean correct = false;
+
+            if (date <= 28) {
+                correct = true;
+            }
+
+            else {
+                System.out.println(">> " + year + " is a leap year, February can only have 28 days");
+            }
+
+        return correct;
+    }
+
+    /**
+     * This method handles if the date a user has given is correct
+     * to the amount of days in a leap year 29 day February. It is
+     * used as a part of the testFullDate method.
+     *
+     * @param year Test the year due date of the task
+     * @param date Test the date of the due date of the task
+     * @return Will return true, if parameters are valid
+     */
+
+    public boolean handlesLeapFebruary (int year, int date) {
+        boolean correct = false;
+
+        if (date <= 29) {
+            correct = true;
+        }
+
+        else {
+            System.out.println(">> " + year + " is a leap year, February can only have 29 days");
+        }
+
+        return correct;
+    }
+
+    /**
+     * This method handles if the date a user has given is correct
+     * to the amount of days in the months of a year (excluding
+     * February). It is used as a part of the testFullDate method.
+     *
+     * @param month Test the month due date of the task
+     * @param date Test the date of the due date of the task
+     * @return Will return true, if parameters are valid
+     */
+
+    public boolean handlesMonthsAndDays (int month, int date)
         {
-            if (month == february)
-            {
-                if (date <= 29)
-                {
+            boolean correct = false;
+            Calendar tempCal = Calendar.getInstance();
+
+            if (monthsWithThirtyDays.contains(month)) {
+
+                if (date < 31) {
                     correct = true;
                 }
 
                 else {
-                        System.out.println(">> " + year + " is a leap year, February can only have 29 days");
+                    tempCal.set(Calendar.MONTH, month);
+
+                    System.out.println(">> There are only 30 days in " +
+                            tempCal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()));
                 }
             }
-
+            // Month contains 31 days
             else {
 
-                if (monthsWithThirtyDays.contains(month))
-                {
-                    if (date < 31)
-                    {
-                        correct = true;
-                    }
-
-                    else
-                    {
-                        tempCal.set(Calendar.MONTH, month);
-                        System.out.println(">> There are only 30 days in " + tempCal.get() );
-                    }
+                if (date <= 31) {
+                    correct = true;
                 }
 
+                else {
+                    tempCal.set(Calendar.MONTH, month);
+
+                    System.out.println(">> There are only 31 days in " +
+                            tempCal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()));
+                }
+            }
+            return correct;
+        }
+
+    /**
+     * A method to populate an ArrayList of the leap years
+     * within the bounds of acceptable due date years. Some
+     * methods in this class will check this collection to
+     * perform their function.
+     */
+
+    public void populateLeapYears ()
+        {
+            leapYears = new ArrayList<>();
+
+            for (int i = 2000; i < 2100; i += 4) {
+                leapYears.add(i);
             }
         }
 
-    }
+    /**
+     * A method to populate an ArrayList of the months with
+     * thirty days in them. Some methods in this class will
+     * check this collection to perform their function.
+     */
 
-    public void populateLeapYears()
-    {
-        leapYears = new ArrayList<>();
-
-        for (int i = 2000; i < 2100; i += 4)
+    public void populateThirtyDayMonths ()
         {
-            leapYears.add(i);
-        }
-
-        for (Integer all : leapYears)
-        {
-            System.out.println(all);
-        }
-
-    }
-
-    public void populateThirtyDayMonths()
-        {
+            monthsWithThirtyDays = new ArrayList<>();
             monthsWithThirtyDays.add(3);
             monthsWithThirtyDays.add(5);
             monthsWithThirtyDays.add(8);
             monthsWithThirtyDays.add(10);
         }
+
+    /**
+     * A method to print out information on the dueDate field
+     * related to a task. It works in tandem with the checkIfDefault
+     * method to confirm if a dueDate is at its default state, or if
+     * it has been set by the user. It will shift the month by 1 when
+     * printed to the user, as the Calendar class has a field for
+     * month which starts at 0. It will also add a 0 to the date/month
+     * , if the date is less than 10, such that the String fits a
+     * YYYY-MM-DD format.
+     *
+     * @return A String based on the dueDate of a task
+     */
+
+    public String printDateString() {
+
+        String dateString;
+        String monthString;
+        String result;
+
+        if (checkIfDefaultDate()) {
+
+            result = "Not set";
+        }
+
+        else {
+
+            int date = dueDate.get(Calendar.DATE);
+
+            if (date < 10) {
+                dateString = "0" + date;
+            }
+
+            else {
+                dateString = date + "";
+            }
+
+            int month = dueDate.get(Calendar.MONTH) + 1;
+
+            if (month < 10) {
+                monthString = "0" + month;
+            }
+
+            else {
+                monthString = month + "";
+            }
+
+            result = dueDate.get(Calendar.YEAR) + "-" +
+                    monthString + "-" + dateString;
+
+        }
+
+        return result;
+    }
 
 }
